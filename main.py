fbfc7a0482811d26bd8cb4a9b28492712f15a408
kelыч <kltodzd999lit@yandex.ru>
2025-07-31 16:12:20 +0300

    исправил main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import uvicorn
+from app.API import api
+from fastapi import FastAPI
+
+app = FastAPI()
+
+app.include_router(api.router)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app")
+    uvicorn.run(app)
